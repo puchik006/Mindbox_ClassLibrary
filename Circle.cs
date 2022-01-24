872107bf55ce44817fa541f23f203a4dacefc2b7
Pavel_Bu <puchik006@mail.ru>
2022-01-24 23:03:27 +0300

    Add Circle class, separate classes

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mindbox_ClassLibrary
+{
+    public class Circle: IFigure
+    {
+        private float _radius;
+
+        public Circle (float radius)
+        {
+            _radius = MathF.Abs(radius);
+        }
+
+        public float GetPerimeter()
+        {
+            return 2 * MathF.PI * _radius;
+        }
+
+        public float GetSquare()
+        {
+            return MathF.PI * _radius * _radius;
+        }
+    }
+}
--- Triangle.cs
+++ Triangle.cs
@@ -2,19 +2,6 @@
 
 namespace Mindbox_ClassLibrary
 {
-    //Напишите на C# библиотеку для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам.
-    //Дополнительно к работоспособности оценим:
-    //Юнит-тесты
-    //Легкость добавления других фигур
-    //Вычисление площади фигуры без знания типа фигуры в compile-time
-    //Проверку на то, является ли треугольник прямоугольным
-
-    interface IFigure
-    {
-        public float GetSquare();
-        public float GetPerimeter();
-    }
-
     public class Triangle : IFigure
     {
         private float _sideA;
@@ -85,10 +72,4 @@ namespace Mindbox_ClassLibrary
         }
 
     }
-
-    public class Circle
-    {
-        private float _radius;
-    }
-        
 }
