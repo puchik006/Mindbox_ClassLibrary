fe4c43399f2af48c7deacdd44c0030686ad632b7
Pavel_Bu <puchik006@mail.ru>
2022-01-24 20:30:52 +0300

    Tried to solve task by using interface, looks like wrong solution

--- Class1.cs
+++ Class1.cs
@@ -2,8 +2,35 @@
 
 namespace Mindbox_ClassLibrary
 {
-    public class Class1
+    //Напишите на C# библиотеку для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам.
+    //Дополнительно к работоспособности оценим:
+    //Юнит-тесты
+    //Легкость добавления других фигур
+    //Вычисление площади фигуры без знания типа фигуры в compile-time
+    //Проверку на то, является ли треугольник прямоугольным
+
+    interface IFigure
     {
+        public float Square();
+        public float Perimeter();
+    }
+
+    public class Triangle: IFigure
+    {
+        public float SideA;
+        public float SideB;
+        public float SideC;
+
+        public float Square(float sideA, float sideB, float sideC)
+        {
+            return 5f;
+        }
+
+        public float Perimeter(float sideA, float  sideB, float sideC)
+        {
+            return (sideA + sideB + sideC);
+        }
+
 
     }
 }
